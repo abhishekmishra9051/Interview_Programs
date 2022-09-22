12c642ac6495155995900e263ff6e5a6a464dcf7
Abhishek Mishra <abhishekmishra@gmail.com>
2022-09-23 00:44:25 +0530

    It's my first Commit.

---
+++ src/Fibonacci_Series.java
@@ -0,0 +1,15 @@
+public class Fibonacci_Series {
+    public static void main(String[] args) {
+        int num1 = 0;
+        int num2 = 1;
+        int num3;
+        int count = 10;
+        System.out.print(num1+ " "+num2);
+        for (int i = 2; i<count; i++){
+            num3= num1+num2;
+            System.out.print(" "+num3);
+            num1=num2;
+            num2=num3;
+        }
+    }
+}
