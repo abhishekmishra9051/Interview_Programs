8ad2dfb397b5ddefc274a8cfc07ec899600c9b05
Abhishek Mishra <abhishekmishra@gmail.com>
2022-09-23 01:02:05 +0530

    It is the program to checking for prime number.

---
+++ src/IsPrime.java
@@ -0,0 +1,19 @@
+public class IsPrime {
+    public static void main(String[] args) {
+        int number = 43;
+        boolean flag = false;
+        for(int i = 2; i <= number/2; i++){
+            if(number % i == 0){
+                flag=true;
+                break;
+            }
+        }
+        if(!flag) {
+            System.out.println(number + " is prime ");
+        }
+        else
+        {
+            System.out.println(number+" is not prime");
+        }
+    }
+}
