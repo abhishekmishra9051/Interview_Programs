0910035cee7b148799030330f5025e9f6347132a
Abhishek Mishra <abhishekmishra9051@gmail.com>
2022-09-23 11:38:11 +0530

    It is the program of Checking number is prime or not.

--- src/IsPrime.java
+++ src/IsPrime.java
@@ -1,6 +1,6 @@
 public class IsPrime {
     public static void main(String[] args) {
-        int number = 5;
+        int number = 43;
         boolean flag = false;
         for(int i = 2; i <= number/2; i++){
             if(number % i == 0){
